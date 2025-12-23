0c4df6f86f338dcb66bc84daa81885a1e8132bbe
obrubovstas <30770015+obrubovstas@users.noreply.github.com>
2025-12-23 17:35:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -183,28 +183,28 @@ def load_conversions(file):
 
     st.write(f"🧪 conversions aggregated rows: {len(merged):,}")
 
-        with engine.begin() as conn:
-        st.write("🧪 before TRUNCATE conv staging")
-        conn.execute(text("truncate staging_conversions_daily;"))
-        st.write("🧪 after TRUNCATE conv staging")
+    with engine.begin() as conn:
+    st.write("🧪 before TRUNCATE conv staging")
+    conn.execute(text("truncate staging_conversions_daily;"))
+    st.write("🧪 after TRUNCATE conv staging")
 
-        st.write("🧪 before COPY conv to staging")
-        copy_df_to_table(conn, merged[["day", "subid", "leads", "sales"]], "staging_conversions_daily")
-        st.write("🧪 after COPY conv to staging")
+    st.write("🧪 before COPY conv to staging")
+    copy_df_to_table(conn, merged[["day", "subid", "leads", "sales"]], "staging_conversions_daily")
+    st.write("🧪 after COPY conv to staging")
 
-        st.write("🧪 before MERGE conv to fact")
-        conn.execute(text("""
-            insert into fact_conversions_daily(day, subid, leads, sales)
-            select day, subid, leads, sales
-            from staging_conversions_daily
-            on conflict (day, subid)
-            do update set
-              leads = excluded.leads,
-              sales = excluded.sales;
-        """))
-        st.write("🧪 after MERGE conv to fact")
+    st.write("🧪 before MERGE conv to fact")
+    conn.execute(text("""
+        insert into fact_conversions_daily(day, subid, leads, sales)
+        select day, subid, leads, sales
+        from staging_conversions_daily
+        on conflict (day, subid)
+        do update set
+            leads = excluded.leads,
+            sales = excluded.sales;
+    """))
+    st.write("🧪 after MERGE conv to fact")
 
-    st.write("🎉 conversions загружены")
+st.write("🎉 conversions загружены")
 
 # ---------------- UI ----------------
 st.title("📊 KT dashboard")
